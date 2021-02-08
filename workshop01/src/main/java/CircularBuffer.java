a8481cbd213222d0a82ee3511fc6bc97f500b30a
Tharawat Moungsouy <tharawat.m@kbtg.tech>
2021-02-08 12:58:03 +0700

    add feature

--- workshop01/src/main/java/CircularBuffer.java
+++ workshop01/src/main/java/CircularBuffer.java
@@ -1,3 +1,63 @@
+import jdk.internal.joptsimple.internal.Strings;
+
+import java.util.Arrays;
+
 public class CircularBuffer {
+
+    private String[] buffers;
+
     // TODO
+    public void create(){// default size = 10
+        buffers = new String[10];
+    }
+    public void create(int size) {
+        buffers = new String[size];
+    }
+    public void write(String input){
+        try{
+
+        }catch (Exception e){
+
+        }
+    }
+
+    public String read(){
+        String result = "";
+        try{
+
+        }catch (Exception e){
+
+        }
+        return result;
+    }
+
+    public int getSize(){
+        int result = 0;
+        try{
+
+        }catch (Exception e){
+
+        }
+        return result;
+    }
+
+    public boolean isEmpty(){
+        boolean result = false;
+        try{
+
+        }catch (Exception e){
+
+        }
+        return result;
+    }
+
+    public boolean isFull(){
+        boolean result = false;
+        try{
+
+        }catch (Exception e){
+
+        }
+        return result;
+    }
 }
